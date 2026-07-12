81a28d4e380c08c79abaf8dd68ea06ba86d29169
Vansh Rawat <vanshrawat643@gmail.com>
2026-07-12 11:53:21 +0000

    created functions for auth

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     database_url: str = "postgresql://postgres:postgres@localhost:5432/editor_db"
-    secret_key: str = "change-me"
+    secret_key: str = "SECRET KEY"
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 60
 
--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,17 @@
+import bcrypt
 from app.core.config import settings
-from datetime import datetime
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from datetime import datetime, timezone, timedelta
+from jose import jwt
+
+def hash_password(password: str) -> str:
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+
+def verify_password(plain: str, hashed: str) -> bool:
+    return bcrypt.checkpw(plain.encode(), hashed.encode())
+
+def create_access_token(data : dict )-> str: 
+    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.access_token_expire_minutes)
+    return jwt.encode({**data, "exp": expire}, settings.secret_key, algorithm=settings.algorithm)
+
+def decode_token(token : str) -> dict: 
+    return jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
